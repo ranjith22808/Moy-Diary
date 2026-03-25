c0d1d24e7e6012d4e9c083a02717c650201d6ccb
ranjith22808 <ranjith22808@gmail.com>
2026-03-25 10:47:23 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'moy-diary-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
